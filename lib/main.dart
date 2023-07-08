c5d2cec8c36c5ce7d6941c24d04a50833a3d1fee
Vasu Gupta <98811369+Vasugupta13@users.noreply.github.com>
2023-07-09 04:33:34 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:ricoz_homescreen/homescreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
